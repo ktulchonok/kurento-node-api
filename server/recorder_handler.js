685f556bae4b7ff4e575e99d0dae2c138898ae71
Max Rostov <ktulchonok@gmail.com>
2018-05-10 15:51:27 +0300

    remove converter

--- server/recorder_handler.js
+++ server/recorder_handler.js
@@ -3,7 +3,6 @@ const _ = require('lodash');
 const fs = require('fs');
 const url = require('url');
 const recUri = `file:///tmp/rec.webm`;
-const hbjs = require('handbrake-js');
 
 class RecorderHandler extends BaseHandler {
 
@@ -36,71 +35,52 @@ class RecorderHandler extends BaseHandler {
           // fs.unlink(url.parse(client.recorder.getUri()).path, _.noop);
         });
         client.recorder.release();
-        hbjs.spawn({
-            input: '/tmp/rec.webm',
-            output: 'static/rec.m4v'
-          })
-          .on('error', err => {
-            // invalid user input, no video found etc
-          })
-          .on('progress', progress => {
-            console.log(
-              'Percent complete: %s, ETA: %s',
-              progress.percentComplete,
-              progress.eta
-            )
-          })
-          .on('complete', () => {
-            console.log('complete!');
-            // setTimeout(() => {
-            //   fs.unlink('static/rec.webm');
-            // }, 5000)
-          })
-      // fs.createReadStream('/tmp/rec.webm').pipe(fs.createWriteStream('static/rec.webm'));
-      // setTimeout(() => {
-      //   fs.unlink('static/rec.webm');
-      // }, 5000)
+        fs.createReadStream('/tmp/rec.webm').pipe(fs.createWriteStream('static/rec.webm'));
+        setTimeout(() => {
+          fs.unlink('static/rec.webm');
+        }, 5000)
+      }
     }
   }
-}
 
-async connect() {
+  async connect() {
 
-  let [client] = this.clients;
-  try {
-    let rtc = client.endpoint;
-    let rec = await this.pipeline.create('RecorderEndpoint', {
-      uri: recUri,
-      stopOnEndOfStream: true,
-      mediaProfile: 'WEBM'
-    });
+    let [client] = this.clients;
+    try {
+      console.log('client id: ', client.id);
+      let rtc = client.endpoint;
+      let rec = await this.pipeline.create('RecorderEndpoint', {
+        uri: recUri,
+        stopOnEndOfStream: true,
+        mediaProfile: 'WEBM'
+      });
 
-    // let webRtc = await this.pipeline.create('WebRtcEndpoint');
-    // let player = await this.pipeline.create("PlayerEndpoint", {
-    //   uri: `file:///tmp/rec.webm`
-    // })
-    // player.on('EndOfStream', function (event) {
-    //   console.log('endOfStream');
-    //   client.player.release();
-    // });
+      // let webRtc = await this.pipeline.create('WebRtcEndpoint');
+      // let player = await this.pipeline.create("PlayerEndpoint", {
+      //   uri: `file:///tmp/rec.webm`
+      // })
+      // player.on('EndOfStream', function (event) {
+      //   console.log('endOfStream');
+      //   client.player.release();
+      // });
 
-    await rtc.connect(rec);
-    await rec.record();
-    client.recorder = rec;
+      await rtc.connect(rec);
+      await rec.record();
+      client.recorder = rec;
 
-    // await rtc.connect(player);
-    // await player.connect(webRtc);
-    // await webRtc.connect(rtc);
-    // await rtc.connect(webRtc);
-    // await player.play();
-    // client.player = player;
+      // await rtc.connect(player);
+      // await player.connect(webRtc);
+      // await webRtc.connect(rtc);
+      // await rtc.connect(webRtc);
+      // await player.play();
+      // client.player = player;
 
-    console.log('Connected!');
+      console.log('Connected!');
 
-  } catch (error) {
-    console.error(`Error connecting media workflow`, error);
+    } catch (error) {
+      console.error(`Error connecting media workflow`, error);
+    }
   }
-}
 }
 
 module.exports = RecorderHandler;
