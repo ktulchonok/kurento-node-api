e9d55c28fa09a36f51264f047c2d5225d35de9ab
Max Rostov <ktulchonok@gmail.com>
2018-07-16 15:26:03 +0300

    add aws-sdk; upload video to s3

--- server/recorder_handler.js
+++ server/recorder_handler.js
@@ -2,10 +2,16 @@ const BaseHandler = require('./base_handler');
 const _ = require('lodash');
 const fs = require('fs');
 const hbjs = require('handbrake-js');
+const AWS = require('aws-sdk');
 const videoDir = '/tmp/kurento-video';
+const s3Bucket = new AWS.S3({
+  params: {
+    Bucket: '321jobs',
+    timeout: 6000000
+  }
+});
 
 class RecorderHandler extends BaseHandler {
-
 
   constructor(kurentoClient, id) {
     super(kurentoClient, id);
@@ -23,6 +29,25 @@ class RecorderHandler extends BaseHandler {
     }
   }
 
+  uploadFile(fileName) {
+    fs.readFile(`/${videoDir}/${fileName}`, function (err, fileData) {
+      if (err) return console.log(err);
+
+      let params = {
+        ACL: 'public-read',
+        Key: fileName,
+        Body: fileData,
+        ContentType: 'binary'
+      };
+
+      s3Bucket.putObject(params, (error, data) => {
+        if (error) return console.log(error);
+
+        console.log("Successfully uploaded %s", fileName)
+      });
+    });
+  }
+
   removeClient(id) {
     const client = _.find(this.clients, {
       id
@@ -36,24 +61,26 @@ class RecorderHandler extends BaseHandler {
         client.recorder.release();
         const videoID = this.id;
         hbjs.spawn({
-          input: `/${videoDir}/${videoID}.webm`,
-          output: `/${videoDir}/${videoID}.mp4`
-        })
-        .on('error', err => {
-          // invalid user input, no video found etc
-          console.error('%s.mp4 \nConverter error: ', err)
-        })
-        .on('progress', progress => {
-          console.log(
-            '%s.mp4 \nPercent complete: %s, ETA: %s',
-            videoID,
-            progress.percentComplete,
-            progress.eta
-          )
-        })
-        .on('complete', () => {
-          console.log('%s.mp4 complete!', videoID);
-        })
+            input: `/${videoDir}/${videoID}.webm`,
+            output: `/${videoDir}/${videoID}.mp4`
+          })
+          .on('error', err => {
+            // invalid user input, no video found etc
+            console.error('%s.mp4 \nConverter error: ', err)
+          })
+          .on('progress', progress => {
+            console.log(
+              '%s.mp4 \nPercent complete: %s, ETA: %s',
+              videoID,
+              progress.percentComplete,
+              progress.eta
+            )
+          })
+          .on('complete', () => {
+            console.log('%s.mp4 complete!', videoID);
+            this.uploadFile(`${videoID}.mp4`);
+          })
+        this.uploadFile(`${videoID}.webm`);
       }
     }
   }
